a93918dc96fa93ffcd0380838b31acdde901563e
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-11-15 23:47:09 +0300

    init: install prettier

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -12,10 +12,7 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
 
   return (
     <ThemedView>
-      <TouchableOpacity
-        style={styles.heading}
-        onPress={() => setIsOpen((value) => !value)}
-        activeOpacity={0.8}>
+      <TouchableOpacity style={styles.heading} onPress={() => setIsOpen((value) => !value)} activeOpacity={0.8}>
         <IconSymbol
           name="chevron.right"
           size={18}
