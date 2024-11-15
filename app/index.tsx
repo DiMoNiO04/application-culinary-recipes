b03b58b5bec4a709d80c2a2e58512fe1c940fae8
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-11-16 00:30:33 +0300

    init: clear project and base structure files

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 24 }}>Welcome to the Culinary Recipes App!</Text>
+    </View>
+  );
+};
+
+export default App;
